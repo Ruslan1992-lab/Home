8f1c49840bf8af2dea2077da389e37516b7c1446
Ruslan1992-lab <ruslan.abkhairov92@gmail.com>
2024-08-12 15:16:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print('12 + 89 - 1')
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 , 1000 != 1000.1)
+
+#3rd program
+print('2 * 2 + 2')
+print('2 * (2 + 2)')
+print(('2 * 2 + 2') == ('2 * (2 + 2)'))
+
+#4th program
+s = '123.456'
+first_digit_after_dot = s[s.index('.') + 1]
+print(first_digit_after_dot)
+
+s = '123.456'
+number = float(s)
+result = number * 10
+print(result)
